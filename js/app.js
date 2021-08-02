53de3595136539aba3d2fd025e1215a7072ec497
csw <chenshihwei@pm.me>
2021-08-02 10:33:06 +0800

    feat: theme switcher

--- js/app.js
+++ js/app.js
@@ -8,3 +8,33 @@ setTimeout((err, data) => {
     console.log(`hello css theme ${data}`)
   }
 }, 3000)
+
+const darkButton = document.getElementById('dark')
+const lightButton = document.getElementById('light')
+const solarButton = document.getElementById('solar')
+const body = document.body
+
+darkButton.onclick = () => {
+  body.classList.replace('light', 'dark')
+}
+
+lightButton.onclick = () => {
+  body.classList.replace('dark', 'light')
+}
+solarButton.onclick = () => {
+  if (body.classList.contains('solar')) {
+    body.classList.remove('solar')
+    solarButton.style.cssText = `
+      --bg-solar: var(--yellow);
+    `
+
+    solarButton.innerText = 'solarize'
+  } else {
+    solarButton.style.cssText = `
+      --bg-solar: white;
+    `
+
+    body.classList.add('solar')
+    solarButton.innerText = 'normalize'
+  }
+}
