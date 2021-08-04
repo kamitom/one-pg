99f66efbdf6b90b1a1b43115b5b51b231226873a
csw <chenshihwei@pm.me>
2021-08-04 21:14:45 +0800

    feat: faker name

--- js/event.test.js
+++ js/event.test.js
@@ -1,13 +1,15 @@
 
 import { EventEmitter } from 'events'
+import faker from 'faker'
 const eventEmitter = new EventEmitter()
 
 eventEmitter.on('myTestEventName', () => {
   const dateObject = new Date()
 
-  const amPm = (dateObject.getHours() >= 18) ? 'Good Evening!' : 'Good Morning!!'
+  const fName = faker.name.findName()
+  const amOrPM = (dateObject.getHours() >= 18) ? 'Good Evening!' : 'Good Morning!!'
 
-  console.log(`Halo, ${amPm}`)
+  console.log(`Halo ${fName}, ${amOrPM}`)
 })
 
 eventEmitter.emit('myTestEventName')
