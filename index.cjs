5bb48aa3be3d6b156667d3ca0a71076f4630cab4
csw <chenshihwei@pm.me>
2021-08-06 13:47:34 +0800

    feat: simple express

---
+++ index.cjs
@@ -0,0 +1,12 @@
+const express = require('express')
+const { readFile } = require('fs/promises')
+
+const app = express()
+
+app.get('/', async (req, res) => {
+  res.send(await readFile('./basic.html', 'utf8'))
+
+  // res.send('hello, world')
+})
+
+app.listen(process.env.PORT || 3000, () => console.log('App4 Available on http://localhost:3000'))
