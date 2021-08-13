b862ede847449364a3b1b03710dbde6950937fed
csw <chenshihwei@pm.me>
2021-08-13 10:27:12 +0800

    chore: css opacity test

--- js/utils/alpha-definitionAPI.js
+++ js/utils/alpha-definitionAPI.js
@@ -19,7 +19,7 @@ const getLocationTemperature = async (locationName) => {
 const getOpenWeatherMapAPI = async (openLocation) => {
   console.log('OpenWeatherData processing...')
   const securityOpenLocation = encodeURIComponent(openLocation)
-  const openApiKey = 'fakerAPIKey'
+  const openApiKey = 'fakeAPIkey'
   const openUrl = `https://api.openweathermap.org/data/2.5/weather?q=${securityOpenLocation}&units=metric&appid=${openApiKey}`
 
   try {
